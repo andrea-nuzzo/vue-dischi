a1912fc1908d4c6e122e720f06ab3abf1f99790e
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2021-12-18 17:11:20 +0100

    draft select author

---
+++ src/components/commons/SelectAuthor.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+      <div class="row">
+          <div class="col">
+            <select v-model= "author" class="form-select" aria-label="Default select example" @change="$emit('selectedAuthor', author)">
+                <option value="">Select authors</option>
+                <option v-for="(author, index) in allAuthors" :key="index" :value="author">{{author}}</option>
+            </select>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SelectAuthor',
+
+    props:{
+        allAuthors: Array,
+    },
+
+    data(){
+        return{
+            author:'',
+        }
+    },
+}
+
+</script>
+
+<style lang='scss' scoped>
+
+select{
+    background-color: black;
+    color: rgb(85, 85, 85);
+    border: 1px solid rgb(85, 85, 85);
+}
+</style>
--- src/components/section/AlbumList.vue
+++ src/components/section/AlbumList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <SelectGenre class="mt-5" @selectedGenre="selectedGenre" :allGenres=" allGenres"/>
+    <SelectAuthor class="mt-3" @selectedAuthor="selectedAuthor" :allAuthors=" allAuthors"/>
+
+    <SelectGenre class="mt-3" @selectedGenre="selectedGenre" :allGenres=" allGenres"/>
+
     <div class="container">
       <div class="row p-5 gy-4">
         <div class="col-12 col-sm-6 col-lg-4" v-for="(item, index) in genreFilter" :key="index">
@@ -15,6 +18,7 @@
 import axios from 'axios'
 import AlbumCard from '../commons/AlbumCard.vue'
 import SelectGenre from '../commons/SelectGenre.vue'
+import SelectAuthor from '../commons/SelectAuthor.vue'
 
 export default {
     name: 'AlbumList',
@@ -22,24 +26,31 @@ export default {
     components: {
       AlbumCard,
       SelectGenre,
+      SelectAuthor,
     },
 
   data(){
     return{
       dataAlbum: [],
-      selected: "",
+      selectedG: "",
+      selectedA:"",
       allGenres: [],
+      allAuthors: [],
     }
   },
 
   methods:{
     selectedGenre(payload){
-      this.selected = payload;
+      this.selectedG = payload;
+      console.log(payload)
     },
 
-    dataPresent(){
-      console.log(this.genreFilter)
+    selectedAuthor(payload){
+      this.selectedA = payload;
+      console.log(payload)
     },
+
+
   },
 
   created(){
@@ -49,11 +60,19 @@ export default {
           // Innesto in dataAlbum tutti gli oggetti dell'API
            this.dataAlbum = response.data.response
 
-          //Ciclo per trovare tutti i generi delle canzoni presenti in "dataAlbum"
+          //Ciclo per trovare tutti i generi e gli autori delle canzoni presenti in "dataAlbum"
            for(let i =0; i < this.dataAlbum.length; i++){
+
+             //Trovo tutti i generi e li inserisco nell'array allGenres
             if(!this.allGenres.includes(this.dataAlbum[i].genre)){
               this.allGenres.push(this.dataAlbum[i].genre);
             }
+
+            //Trovo tutti gli autori e li inserisco nell'array allGenres
+            if(!this.allAuthors.includes(this.dataAlbum[i].author)){
+              this.allAuthors.push(this.dataAlbum[i].author);
+            }
+
           }
 
         })
@@ -68,7 +87,7 @@ export default {
     //Funzione che filtra sui generi di musica
     genreFilter(){
       return this.dataAlbum.filter((item) => {
-        return item.genre.includes(this.selected);
+        return item.genre.includes(this.selectedG) && item.author.includes(this.selectedA);
       });
     }
   },
